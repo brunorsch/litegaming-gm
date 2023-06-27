7cc497d1cab94089478128063f62edb6cf3e864d
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2023-06-26 22:31:42 -0300

    Corrigindo ordem do startup para incluir load de fontes no RML como primeiro

--- src/core/client/startup.ts
+++ src/core/client/startup.ts
@@ -1,7 +1,8 @@
+import './rmlui/fonts/index'
+
 import './devtools/index'
 import './menu/index'
 import './rmlui/index'
-import './rmlui/fonts/index'
 import './webview/index'
 import './utils/listeners'
 import './misc'
