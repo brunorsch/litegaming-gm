afd5a7423215a500675c7dd23b411f56f72a8be8
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2023-06-28 03:23:39 -0300

    Removendo log

--- src/core/client/listeners/up-listener.ts
+++ src/core/client/listeners/up-listener.ts
@@ -4,8 +4,6 @@ import { EventosClient } from '@lg-shared/enum/id-eventos'
 import { enviarNotificacao } from '@lg-client/utils/index'
 
 alt.onServer(EventosClient.UP_XP, ([xpGanho, xpAtual]: [number, number]) => {
-    alt.log('Recebido evento de UP: ' + xpGanho + ' ' + xpAtual)
-
     game.playSoundFrontend(-1, 'MP_RANK_UP', 'HUD_FRONTEND_DEFAULT_SOUNDSET', false)
     enviarNotificacao(`Você ganhou +${xpGanho} de XP! (${xpAtual}/6000)`)
 })
