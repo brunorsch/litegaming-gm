ee2724226431a753b63c947ebc691a8e6f4c05bf
Bruno Ribeiro <brunoribeiro.sch@gmail.com>
2023-06-28 21:46:16 -0300

    Add notificação up level

--- src/core/client/listeners/up-listener.ts
+++ src/core/client/listeners/up-listener.ts
@@ -7,3 +7,8 @@ alt.onServer(EventosClient.UP_XP, ([xpGanho, xpAtual]: [number, number]) => {
     game.playSoundFrontend(-1, 'MP_RANK_UP', 'HUD_FRONTEND_DEFAULT_SOUNDSET', false)
     enviarNotificacao(`Você ganhou +${xpGanho} de XP! (${xpAtual}/6000)`)
 })
+
+alt.onServer(EventosClient.UP_NIVEL, ([novoNivel]: [number]) => {
+    game.playSoundFrontend(-1, 'MP_AWARD', 'HUD_FRONTEND_DEFAULT_SOUNDSET', false)
+    enviarNotificacao(`~p~LEVEL UP!~r~ Parabéns! Você subiu para o nível ~y~${novoNivel}~p~!`)
+})
